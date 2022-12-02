52a7fa4d796ad07bdbb2abd16dde31071b92cd61
alexchwong <80015046+alexchwong@users.noreply.github.com>
2022-12-02 21:57:30 +1100

    Add idxstats for ompBAM and htslib

---
+++ R/ompBAM_imports.R
@@ -0,0 +1,14 @@
+#' @useDynLib ompBench, .registration = TRUE
+#' @import Rcpp
+#' @import zlibbioc
+NULL
+
+#' @export
+idxstats_ompBAM <- function(bam_file, n_threads, verbose = TRUE) {
+    idxstats_pbam(bam_file, n_threads, verbose)
+}
+
+#' @export
+idxstats_htslib <- function(bam_file, n_threads, verbose = TRUE) {
+    idxstats_hts(bam_file, n_threads, verbose)
+}
